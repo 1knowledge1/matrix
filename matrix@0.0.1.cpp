9cf5e7ab8b8da03236db095393a45e148d6c8d38
Yaroslav <32066631+1knowledge1@users.noreply.github.com>
2018-03-03 19:18:11 +0300

    Update matrix@0.0.1.cpp

--- matrix@0.0.1.cpp
+++ matrix@0.0.1.cpp
@@ -10,16 +10,41 @@ class matrix_t {
 	unsigned int columns;
 
 public:
+	// Destructor
+	~matrix_t() {
+		for (unsigned int i = 0; i < rows; i++) {
+			delete[] data[i];
+		}
+		delete[] data;
+	}
+
+	// Constructor
+	matrix_t(unsigned int rows = 0, unsigned int columns = 0) {
+		this->rows = rows;
+		this->columns = columns;
+	}
+
+	// Copy constructor
+	matrix_t(const matrix_t & object) {
+		rows = object.rows;
+		columns = object.columns;
+		data = new int *[rows];
+		for (unsigned int i = 0; i < rows; ++i) {
+			data[i] = new int[columns];
+			for (unsigned int j = 0; j < columns; ++j) {
+				data[i][j] = object.data[i][j];
+			}
+		}
+	}
+
 	matrix_t add(matrix_t & other) {
 		if (!(rows == other.rows && columns == other.columns))
 		{
 			cout << "An error has occured while reading input data.\n";
 			exit(0);
 		}
-		matrix_t res;
-		res.rows = rows;
-		res.columns = columns;
 
+		matrix_t res(rows, columns);
 		res.data = new int *[rows];
 		for (unsigned int i = 0; i < rows; ++i) {
 			res.data[i] = new int[columns];
@@ -35,10 +60,8 @@ public:
 			cout << "An error has occured while reading input data.\n";
 			exit(0);
 		}
-		matrix_t res;
-		res.rows = rows;
-		res.columns = columns;
 
+		matrix_t res(rows, columns);
 		res.data = new int *[rows];
 		for (unsigned int i = 0; i < rows; ++i) {
 			res.data[i] = new int[columns];
@@ -54,10 +77,8 @@ public:
 			cout << "An error has occured while reading input data.\n";
 			exit(0);
 		}
-		matrix_t res;
-		res.rows = rows;
-		res.columns = other.columns;
 
+		matrix_t res(rows, other.columns);
 		res.data = new int *[rows];
 		for (unsigned int i = 0; i < rows; ++i) {
 			res.data[i] = new int[columns];
@@ -67,12 +88,10 @@ public:
 					res.data[i][j] += (data[i][k] * other.data[k][j]);
 			}
 		}
-			return res;
+		return res;
 	}
-	matrix_t trans(matrix_t & other) {
-		matrix_t res;
-		res.rows = columns;
-		res.columns = rows;
+	matrix_t trans() {
+		matrix_t res(columns, rows);
 		res.data = new int *[columns];
 		for (unsigned int i = 0; i < columns; ++i) {
 			res.data[i] = new int[rows];
@@ -82,18 +101,18 @@ public:
 		}
 		return res;
 	}
-	
+
 	ifstream & read(ifstream & stream) {
 		string line, str;
 		getline(stream, line);
 		int rows, columns;
 		char symbol;
 
-		if (stream >> rows && stream >> symbol && symbol == ',' && stream >> columns);
-		else {
+		if (!(stream >> rows && stream >> symbol && symbol == ',' && stream >> columns))
+		{
 			stream.setstate(ios::failbit);
 			return stream;
-			}
+		}
 		this->rows = rows;
 		this->columns = columns;
 
@@ -108,7 +127,7 @@ public:
 				}
 			}
 		}
-		
+
 		data = new int *[rows];
 		for (unsigned int i = 0; i < rows; ++i) {
 			data[i] = new int[columns];
@@ -135,7 +154,19 @@ public:
 	}
 };
 
-void finput()
+bool read(istream & stream, matrix_t & matrix)
+{
+	string file_name;
+	if (stream >> file_name) {
+		ifstream fin;
+		fin.open(file_name);
+		if((fin.is_open()) && (matrix.read(fin)))
+			return true;
+	}
+	return false;
+}
+
+void creat_files()
 {
 	ofstream fout;
 	fout.open("A.txt");
@@ -155,76 +186,44 @@ void finput()
 	fout.close();
 }
 
-int main() {
+int main()
+{
+	creat_files();
 
-	finput();
-	unsigned int num;
+	matrix_t a;
 	char op;
-	string str, name1;
-	matrix_t A, B, res;
 
-	getline(cin, str);
-
-	for (num = 0; (str[num] != ' ') && (str[num] != '+') && (str[num] != '-') && (str[num] != '*'); num++)
-	{
-		name1 = name1 + str[num];
-	}
-	for (; (str[num] != '+') && (str[num] != '-') && (str[num] != '*') && (str[num] != 'T'); num++);
-	op = str[num];
-	if (op != 'T')
-	{
-		string name2;
-		num++;
-		while (str[num] == ' ') num++;
-		for (; num < str.length(); num++)
-		{
-			name2 += str[num];
+	if (read(cin, a) && cin >> op) {
+		if (op == 'T') {
+			matrix_t res = a.trans();
+			res.write(cout);
 		}
-
-		ifstream fin2;
-		fin2.open(name2);
-		if (!fin2.is_open())
-		{
+		else if (op == '+' || op == '-' || op == '*') {
+			matrix_t b;
+			if (read(cin, b)) {
+				if (op == '+') {
+					matrix_t res = a.add(b);
+					res.write(cout);
+				}
+				else if (op == '-') {
+					matrix_t res = a.sub(b);
+					res.write(cout);
+				}
+				else if (op == '*') {
+					matrix_t res = a.mul(b);
+					res.write(cout);
+				}
+			}
+			else {
+				cout << "An error has occured while reading input data.\n";
+			}
+		}
+		else {
 			cout << "An error has occured while reading input data.\n";
-			return 0;
 		}
-		if (!B.read(fin2))
-		{
-			cout << "An error has occured while reading input data.\n";
-			return 0;
-		}
-		fin2.close();
 	}
-
-	ifstream fin1;
-	fin1.open(name1);
-	if (!fin1.is_open())
-	{
-		cout << "An error has occured while reading input data.\n";
-		return 0;
-	}
-	if (!A.read(fin1))
-	{
-		cout << "An error has occured while reading input data.\n";
-		return 0;
-	}
-
-	switch (op) {
-	case '+': 
-		res = A.add(B);
-		break;
-	case '-': 
-		res = A.sub(B);
-		break;
-	case '*': 
-		res = A.mul(B);
-		break;
-	case 'T': 
-		res = A.trans(B);
-		break; 
-	default: 
+	else {
 		cout << "An error has occured while reading input data.\n";
 	}
-	res.write(cout);
 	return 0;
 }
